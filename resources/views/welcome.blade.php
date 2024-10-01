a1b91a071e63cbbd2cb7fcfc632bacb7d9a51a4d
BAferiel <feriel.benabdelghani@esprit.tn>
2024-10-01 21:10:34 +0200

    integration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,15 +3,12 @@
 @extends('layouts.app')
 <body>
   
-    <!-- Include Navbar -->
-    @include('template/navbar')
+  
 
     <div class="content">
         @yield('content')
         <img class="w-100" src="{{asset('img/carousel-1.jpg')}}" alt="Image">
     </div>
-<!-- Include Navbar -->
-@include('template/footer')
 
 </body>
 </html>
