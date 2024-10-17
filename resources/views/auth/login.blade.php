921074762668b68ff3c7c1f25fe52aef6d4f7f7f
omaymaf <omayma.fersi@esprit.tn>
2024-10-18 01:32:20 +0200

    login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -49,19 +49,14 @@
               </label>
             </div>
             <a href="{{ route('password.request') }}" class="text-body">Mot de passe oublié ?</a>
-            <a href="{{ route('password.request') }}" class="text-body">Mot de passe oublié ?</a>
           </div>
 
           <div class="text-center text-lg-start mt-4 pt-2">
             <button type="submit" class="btn btn-primary btn-lg" style="padding-left: 2.5rem; padding-right: 2.5rem;">
               Connexion
-              Se connecter
             </button>
             <p class="small fw-bold mt-2 pt-1 mb-0">Vous n'avez pas de compte ? 
               <a href="{{ route('register') }}" class="link-danger">Inscrivez-vous</a>
-            <p class="small fw-bold mt-2 pt-1 mb-0"> Vous n'avez pas de compte ? 
-              <a href="{{ route('register') }}" class="link-danger">Registre</a>
-            </p>
           </div>
         </form>
       </div>
